15229e861894e4e62b2157a2e25f74980a15e3c0
SilenceDias <11dias02@mail.ru>
2023-05-20 17:31:41 +0600

    Added UI and functionality

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-const rockButton = document.querySelector('.rockButton');
-const paperButton = document.querySelector('.paperButton');
-const scissorsButton = document.querySelector('.scissorsButton');
+let playerScore = 0;
+let computerScore = 0;
+const playerChoice = document.querySelectorAll(".rockButton");
+const firstCardChoice = document.querySelector("#first");
+const secondCardChoice = document.querySelector("#second");
+const resultContainer = document.querySelector(".resContainer");
 
-
-
-
+function disableButtons(){
+    playerChoice.forEach(elem => {
+        elem.disabled = true;
+    } )
+}
 function getComputerChoice(){
     let choiceNumber =  Math.floor(Math.random() * (4 - 1) + 1);
     let choice = "";
@@ -24,65 +29,95 @@ function getComputerChoice(){
     return choice;
 }
 
-function round(computerSelection, playerSelection){
-    if(computerSelection == "rock" && playerSelection == "paper"){
-        console.log("You won! Paper beats rock!");
-    }
-    else if(computerSelection == "rock" && playerSelection == "scissors"){
-        console.log("You lose! Rock beats scissors") ;
-    }
-    else if(computerSelection == "paper" && playerSelection == "rock"){
-        console.log("You lose! Paper beats rock") ;
-    }
-    else if(computerSelection == "paper" && playerSelection == "scissors"){
-        console.log("You won! Scissors beats paper") ;
-    }
-    else if(computerSelection == "scissors" && playerSelection == "paper"){
-        console.log("You lose! Scissors beats paper") ;
-    }
-    else if(computerSelection == "scissors" && playerSelection == "rock"){
-        console.log("You won! Rock beats scissors") ;
-    }
-    else{
-        console.log("DRAW!!!") ;
-    }
+function restart(){
+    const restartButton = document.createElement('button');
+    restartButton.classList.add('restartButton');
+    restartButton.innerText = "Restart the game";
+    const oldRes = document.querySelector(".result");
+    resultContainer.appendChild(restartButton);
+    restartButton.addEventListener("click", function(){
+        window.location.reload()
+    });
 }
 
-rockButton.addEventListener('click', () => round(getComputerChoice(), "rock"));
-paperButton.addEventListener('click', () => round(getComputerChoice(), "paper"));
-scissorsButton.addEventListener('click', () => round(getComputerChoice(), "scissors"));
+function round(computerSelection, playerSelection){
+    if(document.querySelector(".result")){
+        const oldRes = document.querySelector(".result");
+        oldRes.remove();
+    }
+    if(document.querySelectorAll(".score")){
+        const oldScore = document.querySelectorAll(".score");
+        oldScore.forEach(score => {
+            score.remove();
+        })
+    }
+    const result = document.createElement('div');
+    const scorePlayer = document.createElement('div');
+    scorePlayer.classList.add('score');
+    const scoreComputer = document.createElement('div');
+    scoreComputer.classList.add('score');
+    result.classList.add('result')
+    if(computerSelection == "rock" && playerSelection == "paper"){
+        result.innerText = "You won! Paper beats rock!";
+        playerScore++;
+    }
+    else if(computerSelection == "rock" && playerSelection == "scissors"){
+        result.innerText = "You lose! Rock beats scissors" ;
+        computerScore++;
+    }
+    else if(computerSelection == "paper" && playerSelection == "rock"){
+        result.innerText = "You lose! Paper beats rock" ;
+        computerScore++;
+    }
+    else if(computerSelection == "paper" && playerSelection == "scissors"){
+        result.innerText = "You won! Scissors beats paper";
+        playerScore++;
+    }
+    else if(computerSelection == "scissors" && playerSelection == "paper"){
+        result.innerText = "You lose! Scissors beats paper";
+        computerScore++;
+    }
+    else if(computerSelection == "scissors" && playerSelection == "rock"){
+        result.innerText = "You won! Rock beats scissors" ;
+        playerScore++;
+    }
+    else{
+        result.innerText = "DRAW!!!";
+    }
+    scorePlayer.innerText = playerScore;
+    scoreComputer.innerText = computerScore;
+    firstCardChoice.appendChild(scorePlayer);
+    secondCardChoice.appendChild(scoreComputer);
+    resultContainer.appendChild(result);
+    if(playerScore==5){
+        result.innerText = "You won the game!!!";
+        disableButtons();
+        restart();
+    }
+    else if(computerScore==5){
+        result.innerText = "You lose the game!!!";
+        disableButtons();
+        restart();
+    }
+    
+    // firstCardChoice.parentNode.insertBefore(result, firstCardChoice.nextSibling);
+
+    
+
+}
+
+playerChoice.forEach(button => {
+    button.addEventListener("click", function() {
+        round(getComputerChoice(), button.value )
+    })
+})
 
 // function game(){
-//     let scoreComputer = 0;
-//     let playerScore = 0;
-//     let playerChoice = "";
-//     for(let i = 0; i<5;i++){
-//         playerChoice = prompt("What is your choice?(Rock, paper or scissors)");
-//         playerChoice = playerChoice.toLowerCase();
-//         if(playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors"){
-//             alert("YOU PICKED WRONG! ERROR");
-//             return;
-//         }
-//         let roundMessage = round(getComputerChoice(), playerChoice);
-//         if(roundMessage[4] == 'w'){
-//             playerScore++;
-//         }
-//         else if(roundMessage[4] == 'l'){
-//             scoreComputer++;
-//         }
-//         console.log(roundMessage);
-//         console.log("Your score: " + playerScore + " Computers score: " + scoreComputer);
-//     }
-//     console.log("HERE COMES THE RESULTS!")
-//     if(playerScore>scoreComputer){
-//         console.log("YOU ARE CHAMPION!");
-//     }
-//     else if(playerScore<scoreComputer){
-//         console.log("YOU ARE LOSEER!");
-//     }
-//     else{
-//         console.log("DRAW!!!");
-//     }
+//     const rockButton = document.querySelector('.rockButton');
+//     const paperButton = document.querySelector('.paperButton');
+//     const scissorsButton = document.querySelector('.scissorsButton');
+//     const playerChoice = document.querySelectorAll("rockButton");
+   
 // }
 
-// game();
+//  game();
